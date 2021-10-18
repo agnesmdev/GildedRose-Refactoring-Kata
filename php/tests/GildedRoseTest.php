4641938f234f4a4bbfd7fead76b893ba152c5ab8
agnesmdev <agnesm.dev@gmail.com>
2021-10-18 17:49:05 +0200

    add some tests

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -8,14 +8,87 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
-    public function testFoo(): void
+    public function testQualityNeverNegative(): void
     {
+        // given
         $items = [new Item('foo', 0, 0)];
         $gildedRose = new GildedRose($items);
+
+        // when
         $gildedRose->updateQuality();
-        $this->assertSame('foo', $items[0]->name);
+
+        // then
         $this->assertSame(-1, $items[0]->sell_in);
-        // quality never goes negative
         $this->assertSame(0, $items[0]->quality);
+    }
+
+    public function testQualityDecreases(): void
+    {
+        // given
+        $items = [new Item('foo', 1, 2)];
+        $gildedRose = new GildedRose($items);
+
+        // when
+        $gildedRose->updateQuality();
+
+        // then
+        $this->assertSame(0, $items[0]->sell_in);
+        $this->assertSame(1, $items[0]->quality);
+    }
+
+    public function testQualityDecreasesTwice(): void
+    {
+        // given
+        $items = [new Item('foo', -1, 2)];
+        $gildedRose = new GildedRose($items);
+
+        // when
+        $gildedRose->updateQuality();
+
+        // then
+        $this->assertSame(-2, $items[0]->sell_in);
+        $this->assertSame(0, $items[0]->quality);
+    }
+
+    public function testAgedBrie(): void
+    {
+        // given
+        $items = [new Item('Aged Brie', 1, 1)];
+        $gildedRose = new GildedRose($items);
+
+        // when
+        $gildedRose->updateQuality();
+
+        // then
+        $this->assertSame(0, $items[0]->sell_in);
+        $this->assertSame(2, $items[0]->quality);
+    }
+
+    public function testAgedBriePastSellIn(): void
+    {
+        // given
+        $items = [new Item('Aged Brie', 0, 1)];
+        $gildedRose = new GildedRose($items);
+
+        // when
+        $gildedRose->updateQuality();
+
+        // then
+        $this->assertSame(-1, $items[0]->sell_in);
+        $this->assertSame(3, $items[0]->quality);
+    }
+
+    public function testQualityInf50(): void
+    {
+        // given
+        $items = [new Item('Aged Brie', 0, 50)];
+        $gildedRose = new GildedRose($items);
+
+        // when
+        $gildedRose->updateQuality();
+
+        // then
+        $this->assertSame(-1, $items[0]->sell_in);
+        $this->assertSame(50, $items[0]->quality);
     }
 }
